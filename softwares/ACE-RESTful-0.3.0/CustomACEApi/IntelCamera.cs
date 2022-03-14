83d8b6dd85a68610261ee21531bc4786b29da212
vincent evrard <vinceevrardgpv@gmail.com>
2022-03-14 15:20:04 +0100

    ACE-RESTful-0.3.0

---
+++ softwares/ACE-RESTful-0.3.0/CustomACEApi/IntelCamera.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Intel.RealSense;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+namespace CustomACEAPI
+{
+    class IntelCamera
+    {
+        /* TODO */
+    }
+}
