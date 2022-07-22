8f687cc5bef426806ed812f9123e6cbd41fc4f78
vincent evrard <vinceevrardgpv@gmail.com>
2022-07-22 14:59:29 +0200

    update error handleing

--- perpetuite-III/client-perpetuite-3/release/ErrorDictionnary.js
+++ perpetuite-III/client-perpetuite-3/release/ErrorDictionnary.js
@@ -9,11 +9,11 @@ exports.default = void 0;
   client-perpetuite-3 - ErrorDictionnary.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2022-07-22 14:50:06
-  @Last Modified time: 2022-07-22 14:57:15
+  @Last Modified time: 2022-07-22 14:59:12
 \*----------------------------------------*/
 var _default = Object.freeze({
   unknown: `Unknown error : `,
-  "(-908)": "e-Stop - big red button is pressed"
+  "(-908):": "e-Stop - big red button is pressed"
 });
 
 exports.default = _default;
