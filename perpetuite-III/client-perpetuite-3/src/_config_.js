f6ff10a2bf8745bc5ea8601ad8e3b5acb7b04050
vincent evrard <vinceevrardgpv@gmail.com>
2022-07-22 14:13:33 +0200

    config

--- perpetuite-III/client-perpetuite-3/src/_config_.js
+++ perpetuite-III/client-perpetuite-3/src/_config_.js
@@ -2,14 +2,14 @@
   client-perpetuite-3 - Parameters.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2022-07-21 15:41:58
-  @Last Modified time: 2022-07-22 11:23:17
+  @Last Modified time: 2022-07-22 14:13:12
 \*----------------------------------------*/
 
 
 export default Object.freeze({
 	connection : {
-	   //host: "localhost", 
-	   host: "DESKTOP-8KD5D4K.local", 
+	   host: "localhost", 
+	   //host: "DESKTOP-8KD5D4K.local", 
 	   port: 9090,
 	   timeout: 1000
 	}
