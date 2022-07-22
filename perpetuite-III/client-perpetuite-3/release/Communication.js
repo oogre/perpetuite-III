518d22696a367136b14b1e18128c4adfbcb71f95
vincent evrard <vinceevrardgpv@gmail.com>
2022-07-22 14:12:35 +0200

    update

--- perpetuite-III/client-perpetuite-3/release/Communication.js
+++ perpetuite-III/client-perpetuite-3/release/Communication.js
@@ -15,19 +15,21 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
   client-perpetuite-3 - Communication.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2022-07-21 15:58:08
-  @Last Modified time: 2022-07-22 11:12:58
+  @Last Modified time: 2022-07-22 14:12:07
 \*----------------------------------------*/
 const net = require('net');
 
 var _default = async req => {
   // Request object
-  console.log(req.toString());
+  console.log("call ", req.toString());
   return new Promise((resolve, reject) => {
     if (!(0, _Tools.isRequest)(req)) return reject("Call communication takes only Request Object as parameter ");
     const client = net.createConnection(_config_2.default.connection, () => {
+      console.log("send : ", req.toString());
       client.write(req.toString());
     });
     client.on('data', data => {
+      console.log("received : ", data);
       resolve(data);
       client.end();
     });
--- perpetuite-III/client-perpetuite-3/src/Communication.js
+++ perpetuite-III/client-perpetuite-3/src/Communication.js
@@ -2,7 +2,7 @@
   client-perpetuite-3 - Communication.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2022-07-21 15:58:08
-  @Last Modified time: 2022-07-22 11:12:58
+  @Last Modified time: 2022-07-22 14:12:07
 \*----------------------------------------*/
 
 import _config_ from './_config_.js';
@@ -11,14 +11,16 @@ const net = require('net');
 
 
 export default async (req) => { // Request object
-	console.log(req.toString());
+	console.log("call ", req.toString());
     return new Promise((resolve, reject)=>{
 		if(!isRequest(req))return reject("Call communication takes only Request Object as parameter ");
 		const client = net.createConnection(_config_.connection, () => {
+			console.log("send : ", req.toString());
 			client.write(req.toString());
 		});
 
 		client.on('data', (data) => {
+			console.log("received : ", data);
 			resolve(data);
 			client.end();
 		});
