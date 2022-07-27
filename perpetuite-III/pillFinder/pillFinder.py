f5b11d57027c0266e015d0671f0e5b76e0b1648c
vincent evrard <vinceevrardgpv@gmail.com>
2022-07-27 17:14:01 +0200

    change char

--- perpetuite-III/pillFinder/pillFinder.py
+++ perpetuite-III/pillFinder/pillFinder.py
@@ -16,7 +16,7 @@ custom_transforms = transforms.Compose([
 Train_dataset=core.Dataset('Train/',transform=custom_transforms)#L1
 Test_dataset = core.Dataset('Test/')#L2
 loader=core.DataLoader(Train_dataset, batch_size=2, shuffle=True)#L3
-model = core.Model([‘Pill’])#L4
+model = core.Model(['Pill'])#L4
 losses = model.fit(loader, Test_dataset, epochs=25, lr_step_size=5, learning_rate=0.001, verbose=True)#L5
 
 plt.plot(losses)
