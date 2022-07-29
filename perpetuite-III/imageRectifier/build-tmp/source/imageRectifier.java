b7b7fef9f5478bb3d6443baee4ac9f62f8b779bb
vincent evrard <vinceevrardgpv@gmail.com>
2022-07-29 15:01:26 +0200

    update data

--- perpetuite-III/imageRectifier/build-tmp/source/imageRectifier.java
+++ perpetuite-III/imageRectifier/build-tmp/source/imageRectifier.java
@@ -1,4 +1,22 @@
-import javafx.util.Pair;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import javafx.util.Pair; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class imageRectifier extends PApplet {
+
+
 
 
 PImage src;
@@ -7,12 +25,12 @@ JSONArray data;
 PVector [][] points;
 Triple<Integer, Integer, Integer> [] baseMesh;
 
-void setup() {
+public void setup() {
   src = loadImage("camera.jpg");
   data = loadJSONArray("data.json");
 
 
-  size(760, 760, P3D);
+  
 
   int tagCount = data.size();
   int pointCount = tagCount * 4;
@@ -49,13 +67,13 @@ try{
   }
 }
 
-Pair<Integer, Integer> decompose(int v) {
+public Pair<Integer, Integer> decompose(int v) {
   //println(v, new Pair<Integer, Integer>(v%8, v/8));
   return new Pair<Integer, Integer>(v%8, v/8);
 }
 
 
-void draw() {
+public void draw() {
 
   //println();
   //scale(0.5);
@@ -147,4 +165,25 @@ void draw() {
   //    text(x + y * 8, points[x][y].x, points[x][y].y);
   //  }
   //}
+}
+public class Triple<A, B, C> {
+    public final A x;
+    public final B y;
+    public final C z;
+
+    public Triple(final A x, final B y, final C z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+} 
+  public void settings() {  size(760, 760, P3D); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "imageRectifier" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
