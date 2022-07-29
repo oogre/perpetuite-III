2d2d69a5bd590c7107d554cb900e54d85c547251
vincent <vincent@ogre>
2022-07-29 15:24:57 +0200

    updat img

--- perpetuite-III/pillFinder/usePillFinder.py
+++ perpetuite-III/pillFinder/usePillFinder.py
@@ -10,11 +10,11 @@ image = utils.read_image('Test/rectified.jpg')
 predictions = model.predict(image)
 labels, boxes, scores = predictions
 
-thresh=0.3
+thresh=0.7
 filtered_indices=np.where(scores>thresh)
 filtered_scores=scores[filtered_indices]
 filtered_boxes=boxes[filtered_indices]
 num_list = filtered_indices[0].tolist()
 filtered_labels = [labels[i] for i in num_list]
 
-show_labeled_image(image, boxes, labels)
+show_labeled_image(image, filtered_boxes, filtered_labels)
