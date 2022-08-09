563d6d397253a3d88b437f8de6612e71fe0f812c
vincent <vincent@ogre>
2022-08-09 19:59:32 +0200

    sommechange

--- perpetuite-III/pillFinder/testOpenCV/main.py
+++ perpetuite-III/pillFinder/testOpenCV/main.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np;
 
 # Read image
-im = cv2.imread("../../buildCameraMapperScript/data/out.png", cv2.IMREAD_GRAYSCALE)
+im = cv2.imread("../../buildCameraMapperScript/data/out.jpg", cv2.IMREAD_GRAYSCALE)
 
 # Set up the detector with default parameters.
 detector = cv2.SimpleBlobDetector()
@@ -13,8 +13,11 @@ keypoints = detector.detect(im)
 
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
+
+
+print(keypoints)
 # Show keypoints
-cv2.imshow("Keypoints", im_with_keypoints)
+cv2.imshow("Keypoints", im)
 cv2.waitKey(0)
