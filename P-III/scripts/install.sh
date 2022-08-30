9638845f2eef856f6d8f61b8fc0ddf49c91ce68d
vincent evrard <vinceevrardgpv@gmail.com>
2022-08-30 13:19:49 +0200

    compleate install

--- P-III/scripts/install.sh
+++ P-III/scripts/install.sh
@@ -2,7 +2,7 @@
 # @Author: Evrard Vincent
 # @Date:   2022-08-19 18:07:45
 # @Last Modified by:   vincent evrard
-# @Last Modified time: 2022-08-22 19:54:00
+# @Last Modified time: 2022-08-24 18:17:54
 
 if [ $EUID != 0 ]; then
 	sudo "$0" "$@"
@@ -25,5 +25,11 @@ ln -s $(pwd)/release/robot/highLevelAPI/P-III.goHome /usr/local/bin/P-III.goHome
 
 echo "create symLink /usr/local/bin/P-III.go"
 ln -s $(pwd)/release/robot/highLevelAPI/P-III.go /usr/local/bin/P-III.go
+
+echo "create symLink /usr/local/bin/P-III.grab"
+ln -s $(pwd)/release/robot/highLevelAPI/P-III.grab /usr/local/bin/P-III.grab
+
+echo "create symLink /usr/local/bin/P-III.cv.rectify"
+ln -s $(pwd)/release/robot/highLevelAPI/P-III.cv.rectify /usr/local/bin/P-III.cv.rectify
 
 echo 'export PIII_PATH='$(pwd) >> ~/.profile
