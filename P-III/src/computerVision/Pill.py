44b00d5e1aef38b934d8423bea73b4e8d9ab0d25
perpetuite3 <vincent@ogre.be>
2023-02-01 10:36:16 +0100

    cv + isPill

--- P-III/src/computerVision/Pill.py
+++ P-III/src/computerVision/Pill.py
@@ -19,22 +19,26 @@ class Pill:
         # self.perimeter = cv2.arcLength(contour, True)
         # self.circularity = getCircularity(self.area, self.perimeter)
         # print(self.area)
-        if self.area > 2200 and self.area < 4200: # and self.circularity > 0.75 :
-            self.centroid = (M["m10"]/M["m00"], M["m01"]/M["m00"])
 
-            x, y, w, h = cv2.boundingRect(contour)
-            self.bBox = {"x":x, "y":y, "w":w, "h":h}
+        self.isPill= self.area > 2200 and self.area < 4200: # and self.circularity > 0.75 :
+ 
+        self.centroid = (M["m10"]/M["m00"], M["m01"]/M["m00"])
 
-            parcel = imBGR[self.bBox["y"]:self.bBox["y"]+self.bBox["h"], self.bBox["x"]:self.bBox["x"]+self.bBox["w"]]
-            pixels = np.float32(parcel.reshape(-1, 3))
-            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
-            _, labels, palette = cv2.kmeans(pixels, 2, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-            _, counts = np.unique(labels, return_counts=True)
-            [b, g, r] = palette[np.argmax(counts)]
-            self.avgRGB = (int(b), int(g), int(r))
+        x, y, w, h = cv2.boundingRect(contour)
+        self.bBox = {"x":x, "y":y, "w":w, "h":h}
+
+        parcel = imBGR[self.bBox["y"]:self.bBox["y"]+self.bBox["h"], self.bBox["x"]:self.bBox["x"]+self.bBox["w"]]
+        pixels = np.float32(parcel.reshape(-1, 3))
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
+        _, labels, palette = cv2.kmeans(pixels, 2, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        _, counts = np.unique(labels, return_counts=True)
+        [b, g, r] = palette[np.argmax(counts)]
+        self.avgRGB = (int(b), int(g), int(r))
     def toObj(self):
         return {
             "center" : [float("{:.2f}".format(self.centroid[0])), float("{:.2f}".format(self.centroid[1]))],
             "avgRGB" : [self.avgRGB[2], self.avgRGB[1], self.avgRGB[0]],
-            "box" : [self.bBox["x"],self.bBox["y"], self.bBox["w"], self.bBox["h"]]
+            "box" : [self.bBox["x"],self.bBox["y"], self.bBox["w"], self.bBox["h"]],
+            "isPill" : self.isPill,
+            "area" : self.area
         }
