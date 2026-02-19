79f2fd6c4a13af0b4958d68dd1d1871c5c14c47d
Matière humaine <matiere@humaine.tv>
2026-02-19 11:37:38 +0100

    unlocking process

--- simulator/P-II/src/API/Memory/lock.js
+++ simulator/P-II/src/API/Memory/lock.js
@@ -1,15 +1,31 @@
 import {Memory_get} from "./get.js";
+import {isLocation, isBox, isString} from "./../../tools/validators.js";
+import {delay} from "./../../tools/helpers.js";
 
 
-export const Memory_lock = ({memory}, location)=>{
-	const pill = Memory_get({memory}, location);
-	if(pill){
-		pill.lock();
+export const Memory_lock = async ({memory}, location)=>{
+	if(isLocation(location)){
+		const pill = Memory_get({memory}, location);
+		if(pill){
+			pill.lock();
+		}
+	}else{
+		for(const pill of memory.set.toArray()){
+			await delay(Math.random()*50);
+			pill.lock();
+		}
 	}
 }
 
 
 export const Memory_unlock = ({memory}, colorName)=>{
 	const pills = Memory_get({memory}, colorName);
-	pills.map(pill=>pill.unlock())
+
+	const coloredPillHistory = pills.toArray()
+								.filter(({isLock})=>isLock)
+								.sort((pillA, pillB)=>{
+									return pillA.lockedAt - pillB.lockedAt;
+								});
+	coloredPillHistory.length = Math.floor(coloredPillHistory.length/3);
+	coloredPillHistory.map(pill=>pill.unlock());
 }
